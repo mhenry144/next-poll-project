f8c3650dec1ad00e4efbcd2f1a0e8f3d0293004d
Matthew Henry <matt.henry144@gmail.com>
2022-04-14 16:48:32 -0400

    added hover animation

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import SidebarLink from './SidebarLink'
+import { HomeIcon } from '@heroicons/react/solid'
 import {
   HashtagIcon,
   BellIcon,
@@ -19,18 +20,19 @@ function Sidebar() {
         {/* always have to configure the host of the image (domain) */}
         {/* have to specify width height for next images*/}
         <Image src="https://rb.gy/ogau5a" width={30} height={30} />
-        {/*spade = spacing 2.5 px each*/}
-        <div className="spade-y-2.5 mt-4 mb-2.5 xl:ml-24">
-          <SidebarLink text="Home" Icon={HomeIcon} active />
-          <SidebarLink text="Explore" Icon={HashtagIcon} />
-          <SidebarLink text="Notifications" Icon={BellIcon} />
-          <SidebarLink text="Messages" Icon={InboxIcon} />
-          <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
-          <SidebarLink text="Lists" Icon={ClipboardListIcon} />
-          <SidebarLink text="Profile" Icon={UserIcon} />
-          <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
-        </div>
       </div>
+      {/*spade = spacing 2.5 px each*/}
+      <div className="spade-y-2.5 mt-4 mb-2.5 xl:ml-24">
+        <SidebarLink text="Home" Icon={HomeIcon} active />
+        <SidebarLink text="Explore" Icon={HashtagIcon} />
+        <SidebarLink text="Notifications" Icon={BellIcon} />
+        <SidebarLink text="Messages" Icon={InboxIcon} />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarLink text="Lists" Icon={ClipboardListIcon} />
+        <SidebarLink text="Profile" Icon={UserIcon} />
+        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+      <button>Tweet</button>
     </div>
   )
 }
--- components/SidebarLink.js
+++ components/SidebarLink.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useRouter } from 'next/router'
 
 function SidebarLink({ Icon, text, active }) {
-  return <div>Sidebar Link</div>
+  const router = useRouter()
+  return (
+    <div
+      className={`hoverAnimation flex items-center justify-center space-x-3 text-xl text-[#d9d9d9] xl:justify-start ${
+        active && 'font-bold'
+      }`}
+      onClick={() => active && router.push('/')}
+    >
+      <Icon className="h-7" />
+      <span className="hidden xl:inline">{text}</span>
+    </div>
+  )
 }
 
 export default SidebarLink
