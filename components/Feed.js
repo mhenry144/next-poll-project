ad830cc8eef7885a96f280b37e27ad68f3f7b87c
Matthew Henry <matt.henry144@gmail.com>
2022-04-14 18:41:40 -0400

    basic input added

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SparklesIcon } from '@heroicons/react/outline'
+import Input from './Input'
 
 function Feed() {
   // flex-grow will take up as much room as possible
@@ -7,10 +8,14 @@ function Feed() {
     <div className="max-w-2xl flex-grow border-l border-r border-gray-700 text-white sm:ml-[73px] xl:ml-[370px]">
       <div className="sticky top-0 z-50 flex items-center border-b border-gray-700 bg-black py-2 px-3 text-[#d9d9d9] sm:justify-between">
         <h2 className="text-lg font-bold sm:text-xl">Home</h2>
-        <div>
+        <div className="hoverAnimation flex h-9 w-9 items-center justify-center xl:px-0">
           <SparklesIcon className="h-5 text-white" />
         </div>
       </div>
+
+      {/* Input */}
+      <Input />
+      {/* POSTS */}
     </div>
   )
 }
---
+++ components/Input.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Input() {
+  return (
+    <div
+      className={`flex space-x-3 overflow-y-scroll border-b border-gray-700 p-3 `}
+    >
+      <img
+        src="https://imgs.search.brave.com/L9p9xnhqEJGC4qyAqnGjXlZlc_BGAfAOL8ou72Jf_6A/rs:fit:384:384:1/g:ce/aHR0cHM6Ly93d3cu/YXBrbWlycm9yLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAx/Ny8wOC81OThiMzU0/NjhiOGYyLTM4NHgz/ODQucG5n"
+        alt=""
+        className="h-11 w-11 rounded-full xl:mr-2.5"
+      />
+      {/* divide y = border and adds it to each child */}
+      <div className="w-full divide-y divide-gray-700">
+        <div>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Input
