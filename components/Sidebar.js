433b1773b5c10350c46db648ab0fcb7f92922d76
Matthew Henry <matt.henry144@gmail.com>
2022-04-14 15:52:51 -0400

    tailwind css init

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,7 +5,13 @@ function Sidebar() {
   return (
     <div className="fixed hidden h-full flex-col items-center p-2 sm:flex xl:w-[340px] xl:items-start">
       <div className="hoverAnimation flex h-14 w-14 items-center justify-center p-0 xl:ml-24">
-        <Image src="" />
+        {/* always have to configure the host of the image (domain) */}
+        {/* have to specify width height for next images*/}
+        <Image src="https://rb.gy/ogau5a" width={30} height={30} />
+        {/*spade = spacing 2.5 px each*/}
+        <div className="spade-y-2.5">
+          <SidebarLink />
+        </div>
       </div>
     </div>
   )
