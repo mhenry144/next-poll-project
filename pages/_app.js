bfa14310da6ba281425b409c202e2cf61297e5ae
Matthew Henry <matt.henry144@gmail.com>
2022-04-17 16:56:40 -0400

    Setting up Next.js Auth

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    // `session` comes from `getServerSideProps` or `getInitialProps`.
+    // Avoids flickering/session loading on first load.
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,48 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
+import { getProviders, getSession, useSession } from 'next-auth/react'
+
+export default function Home() {
+  return (
+    // flex min-h-screen flex-col items-center justify-center py-2
+    <div className="">
+      <Head>
+        <title>Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="mx-auto flex min-h-screen max-w-[1500px] bg-black">
+        <Sidebar />
+        {/* Feed */}
+        <Feed />
+        {/* Widgets */}
+        {/* Modal */}
+      </main>
+    </div>
+  )
+}
+// server side props
+export async function getServerSideProps(context) {
+  // fetch props from server
+  const trendingResults = await fetch('https://jsonkeeper.com/b/NKEV').then(
+    (res) => res.json()
+  )
+  //
+  const followResults = await fetch('https://jsonkeeper.com/b/WWMJ').then(
+    (res) => res.json()
+  )
+
+  const providers = await getProviders()
+  const session = await getSession(context)
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  }
+}
