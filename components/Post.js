1502b3fa0f444cfb5935b1a2160ca2eed7cbf6c5
Matthew Henry <matt.henry144@gmail.com>
2022-04-18 14:26:32 -0400

    working on posts

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+} from '@firebase/firestore'
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  SwitchHorizontalIcon,
+  TrashIcon,
+} from '@heroicons/react/outline'
+import {
+  HeartIcon as HeartIconFilled,
+  ChatIcon as ChatIconFilled,
+} from '@heroicons/react/solid'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import Moment from 'react-moment'
+import { useRecoilState } from 'recoil'
+import { modalState, postIdState } from '../atoms/modalAtom'
+import { db } from '../firebase'
 
-function Post() {
-  return <div>Post</div>
+function Post({ id, post, postPage }) {
+  const { data: session } = useSession()
+  const [isOpen, setIsOpen] = useRecoilState(modalState)
+  const [postId, setPostId] = useRecoilState(postIdState)
+  const [comments, setComments] = useState([])
+  const router = useRouter()
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, 'posts', id, 'comments'),
+          orderBy('timestamp', 'desc')
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db, id]
+  )
+
+  return (
+    <div
+      className="flex cursor-pointer border-b border-gray-700 p-3"
+      onClick={() => router.push(`/${id}`)}
+    >
+      {!postPage && (
+        // post? = option chaining = if undefined do not send error
+        <img
+          src={post?.userImg}
+          alt=""
+          className="mr-4 h-11 w-11 rounded-full"
+        />
+      )}
+    </div>
+  )
 }
 
 export default Post
