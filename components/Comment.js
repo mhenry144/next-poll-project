f64af1fbed9e249b508dfc2fa62923370d960bf4
Matthew Henry <matt.henry144@gmail.com>
2022-04-19 15:41:23 -0400

    Update Comment.js

--- components/Comment.js
+++ components/Comment.js
@@ -40,6 +40,26 @@ function Comment({ comment }) {
           <div className="icon group flex-shrink-0">
             <DotsHorizontalIcon className="h-5 text-[#6e767d] group-hover:text-[#1d9bf0]" />
           </div>
+          {/* Option Icons*/}
+          <div className="flex w-10/12 justify-between text-[#6e767d]">
+            <div className="icon group">
+              <ChatIcon className="h-5 group-hover:text-[#1d9bf0]" />
+            </div>
+
+            <div className="group flex items-center space-x-1">
+              <div className="icon group-hover:bg-pink-600/10">
+                <HeartIcon className="h-5 group-hover:text-pink-600" />
+              </div>
+              <span className="text-sm group-hover:text-pink-600"></span>
+            </div>
+
+            <div className="icon group">
+              <ShareIcon className="h-5 group-hover:text-[#1d9bf0]" />
+            </div>
+            <div className="icon group">
+              <ChartBarIcon className="h-5 group-hover:text-[#1d9bf0]" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
