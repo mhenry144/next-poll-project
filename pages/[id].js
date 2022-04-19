60de236edfa69044e2d87685d45d60ab684875d7
Matthew Henry <matt.henry144@gmail.com>
2022-04-19 14:26:51 -0400

    comments page started

--- pages/[id].js
+++ pages/[id].js
@@ -1,7 +1,31 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
+import { useSession } from 'next-auth/react'
+import Modal from '../components/Modal'
+import Sidebar from '../components/Sidebar'
+import Head from 'next/head'
+
+import Post from '../components/Post'
+import { db } from '../firebase'
 
 function PostPage() {
-  return <div>PostPage</div>
+  const [isOpen, setIsOpen] = useRecoilState(modalState)
+  const { data: session } = useSession()
+  return (
+    <div className="">
+      <Head>
+        <title>Home / Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="mx-auto flex min-h-screen max-w-[1500px] bg-black">
+        <Sidebar />
+
+        {isOpen && <Modal />}
+      </main>
+    </div>
+  )
 }
 
 export default PostPage
