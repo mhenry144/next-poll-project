a1cd27558edf35bbcdbfd90dbbdcbe7128620e83
Matthew Henry <matt.henry144@gmail.com>
2022-04-19 15:17:40 -0400

    Update [id].js

--- pages/[id].js
+++ pages/[id].js
@@ -1,33 +1,32 @@
-import { useEffect, useState } from 'react'
-import { useRecoilState } from 'recoil'
-import { modalState } from '../atoms/modalAtom'
-import { getProviders, getSession, useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
-import Modal from '../components/Modal'
-import Sidebar from '../components/Sidebar'
-import Head from 'next/head'
-import Login from '../components/Login'
-
-import Post from '../components/Post'
-import { db } from '../firebase'
 import {
   collection,
-  deleteDoc,
   doc,
   onSnapshot,
   orderBy,
   query,
-  setDoc,
 } from '@firebase/firestore'
+import { getProviders, getSession, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
+import Modal from '../components/Modal'
+import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
+import Post from '../components/Post'
+import { db } from '../firebase'
+import { ArrowLeftIcon } from '@heroicons/react/solid'
+import Comment from '../components/Comment'
+import Head from 'next/head'
 
 function PostPage({ trendingResults, followResults, providers }) {
   const { data: session } = useSession()
   const [isOpen, setIsOpen] = useRecoilState(modalState)
   const [post, setPost] = useState()
+  const [comments, setComments] = useState([])
   const router = useRouter()
   const { id } = router.query
-  console.log(router)
-
+  // fetch posts
   useEffect(
     () =>
       onSnapshot(doc(db, 'posts', id), (snapshot) => {
@@ -35,21 +34,63 @@ function PostPage({ trendingResults, followResults, providers }) {
       }),
     [db]
   )
-
+  // fetch comments
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, 'posts', id, 'comments'),
+          orderBy('timestamp', 'desc')
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db, id]
+  )
+  // if no session -> show login component
   if (!session) return <Login providers={providers} />
 
   return (
-    <div className="">
+    <div>
       <Head>
+        {/* Dynamic Title Naming */}
         <title>
-          {post?.username} on Twitter: "{post?.text}
+          {post?.username} on Twitter: "{post?.text}"
         </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="mx-auto flex min-h-screen max-w-[1500px] bg-black">
         <Sidebar />
-
+        <div className="max-w-2xl flex-grow border-l border-r border-gray-700 sm:ml-[73px] xl:ml-[370px]">
+          {/* Back to Home Page Button */}
+          <div className="sticky top-0 z-50 flex items-center gap-x-4 border-b border-gray-700 bg-black px-1.5 py-2 text-xl font-semibold text-[#d9d9d9]">
+            <div
+              className="hoverAnimation flex h-9 w-9 items-center justify-center xl:px-0"
+              onClick={() => router.push('/')}
+            >
+              <ArrowLeftIcon className="h-5 text-white" />
+            </div>
+            Tweet
+          </div>
+          {/* Entire tweet displayed here */}
+          <Post id={id} post={post} postPage />
+          {comments.length > 0 && (
+            <div className="pb-72">
+              {/* Show All Available Comments */}
+              {comments.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  id={comment.id}
+                  comment={comment.data()}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+        <Widgets
+          trendingResults={trendingResults}
+          followResults={followResults}
+        />
+        {/* If Comments -> Show Modal Component */}
         {isOpen && <Modal />}
       </main>
     </div>
